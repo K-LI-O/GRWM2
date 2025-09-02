b552a12609b06ef06ce951c9c3359739ead8a718
dawn-whatshappening <rirrimom@gmail.com>
2025-09-02 20:40:28 +0900

    Refactor: 로그인 아이디 검증 로직 url 수정

--- src/main/java/GRWM/backend/controller/AuthenticationController.java
+++ src/main/java/GRWM/backend/controller/AuthenticationController.java
@@ -62,4 +62,22 @@ public class AuthenticationController {
 
     }
 
+
+    /*
+    함수명 : isDuplicateValidation
+    기능 : 200 ok와 함께 isDuplicate 의 bool 값을 전달한다. 반환값이 true 라면 같은 로그인 아이디의 회원이 존재한다.
+    매개변수 : String userId(로그인 아이디입니다)
+    반환값 : ResponseEntity<Boolean>
+
+     */
+    @GetMapping("/check-id/{userId}")
+    public ResponseEntity<Boolean> idDuplicateValidation(@PathVariable String userId){
+        boolean isDuplicate = memberService.findDuplicateLoginId(userId);
+        if(isDuplicate){
+            return ResponseEntity.ok(true);
+        }
+        return ResponseEntity.ok(false);
+    }
+
+
 }
--- src/main/java/GRWM/backend/controller/MemberController.java
+++ src/main/java/GRWM/backend/controller/MemberController.java
@@ -11,24 +11,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/user")
 public class MemberController {
 
-    private final MemberService memberService;
-
-
-    /*
-    함수명 : isDuplicateValidation
-    기능 : 200 ok와 함께 isDuplicate 의 bool 값을 전달한다. 반환값이 true 라면 같은 로그인 아이디의 회원이 존재한다.
-    매개변수 : String userId(로그인 아이디입니다)
-    반환값 : ResponseEntity<Boolean>
-
-     */
-    @GetMapping("/create/check-id/{userId}")
-    public ResponseEntity<Boolean> idDuplicateValidation(@PathVariable String userId){
-        boolean isDuplicate = memberService.findDuplicateLoginId(userId);
-        if(isDuplicate){
-            return ResponseEntity.ok(true);
-        }
-        return ResponseEntity.ok(false);
-    }
 
 
 
