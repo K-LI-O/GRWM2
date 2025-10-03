e158a78fed094ab63bd735d0da28964aad6d31e7
dawn-whatshappening <rirrimom@gmail.com>
2025-10-03 13:23:19 +0900

    chore : Auth Controller 작은 수정 후 원복

--- src/main/java/GRWM/backend/controller/AuthenticationController.java
+++ src/main/java/GRWM/backend/controller/AuthenticationController.java
@@ -62,7 +62,6 @@ public class AuthenticationController {
                 jwt,
                 "Bearer",
                 memberService.findUsernameByLoginId(dto.getLoginId()),
-                memberService.findUserIdByLoginId(dto.getLoginId()),
                 memberService.findUserIdByLoginId(dto.getLoginId())
                 ));
 
