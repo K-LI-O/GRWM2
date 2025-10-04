0320bd66b0414ae34525ec0876952681a935af11
장다원 <102298614+K-LI-O@users.noreply.github.com>
2025-10-05 07:40:41 +0900

    fix ChatMessageCreateDto 오타 수정

--- src/main/java/GRWM/backend/dto/ChatMessageCreateDto.java
+++ src/main/java/GRWM/backend/dto/ChatMessageCreateDto.java
@@ -13,5 +13,5 @@ public class ChatMessageCreateDto {
 
     private Long chatRoomId;
     private String content;
-    private Long conmmunityId; // userId 넘겨주심 됩니도.
+    private Long communityId; // userId 넘겨주심 됩니도.
 }
