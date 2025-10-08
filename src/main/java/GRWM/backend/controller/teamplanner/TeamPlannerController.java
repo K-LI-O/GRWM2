840f3f6efe0a6ca8eafbb471ffa1dcce926b1a4b
dawn-whatshappening <rirrimom@gmail.com>
2025-10-08 19:29:56 +0900

    fix : url 맵핑 어노테이션 추가

--- src/main/java/GRWM/backend/controller/teamplanner/TeamPlannerController.java
+++ src/main/java/GRWM/backend/controller/teamplanner/TeamPlannerController.java
@@ -8,16 +8,13 @@ import GRWM.backend.service.teamplanner.TeamPlannerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/team-planner/")
+@RequestMapping("/api/team-planner")
 public class TeamPlannerController {
 
     private final TeamPlannerService plannerService;
@@ -36,6 +33,7 @@ String profileImage
     return value : ResponseEntity<Long> plannerId 반환;
     */
 
+    @PostMapping("/create")
     public ResponseEntity<Long> createPlanner(@RequestBody TeamPlannerCreateDto dto,
                                               @AuthenticationPrincipal CustomUserDetails userDetails){
 
@@ -50,6 +48,7 @@ String profileImage
     return value : List<TeamPlannerDto>
     */
 
+    @GetMapping("/list")
     public List<TeamPlannerDto> getPlannerList(@AuthenticationPrincipal CustomUserDetails userDetails){
         return plannerService.getPlannerList(userDetails.getUserId());
     }
@@ -62,6 +61,7 @@ String profileImage
     return value : TeamPlannerDto
     */
 
+    @PutMapping("/{plannerId}/update")
     public TeamPlannerDto updatePlanner(@PathVariable Long plannerId, TeamPlannerUpdateDto dto){
         return plannerService.updatePlanner(plannerId, dto);
     }
@@ -74,7 +74,7 @@ String profileImage
     param : Long plannerId
     return value : x
      */
-
+    @DeleteMapping("/{plannerId}/delete")
     public ResponseEntity deletePlanner(@PathVariable Long plannerId) {
         plannerService.deletePlanner(plannerId);
         return ResponseEntity.noContent().build();
