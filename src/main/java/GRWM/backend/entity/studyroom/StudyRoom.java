def7df660d6c873c51f571dcf7fa37a3c7be6d6d
dawn-whatshappening <rirrimom@gmail.com>
2025-11-05 00:12:47 +0900

    fix : 스터디룸 투두 삭제 로직 수정(1)

--- src/main/java/GRWM/backend/entity/studyroom/StudyRoom.java
+++ src/main/java/GRWM/backend/entity/studyroom/StudyRoom.java
@@ -50,7 +50,7 @@ int extensionCount; // 최대 n회 연장 가능하며, 해당 방이 몇 회나
     private String description;
 
     @Builder.Default
-    @OneToMany(cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @OneToMany(mappedBy = "studyRoom", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<StudyRoomMember> members = new ArrayList<>();
     @OneToMany(mappedBy = "studyRoom", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
